e0719b97a5572c8e16e1cedba2189e12d29eab72
stLmpp <gui.stlmpp@hotmail.com>
2021-11-02 19:23:56 -0300

    - added rate-limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from './person/person.module';
 import { config } from 'dotenv';
 import { getTypeormConfig } from './config/ormconfig';
+import { APP_GUARD } from '@nestjs/core';
+import { RateLimiterGuard, RateLimiterModule } from 'nestjs-rate-limiter';
 
 if (process.env.NODE_ENV !== 'production') {
   config();
 }
 
 @Module({
-  imports: [TypeOrmModule.forRoot(getTypeormConfig()), PersonModule],
+  imports: [TypeOrmModule.forRoot(getTypeormConfig()), PersonModule, RateLimiterModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: RateLimiterGuard }],
 })
 export class AppModule {}
